2a2f2a0049e032565e980a09750efb5e83e9d73d
Alan Sanchez <sanchez2597asp@outlook.com>
2015-07-04 23:59:02 -0500

    Fixed problem with .min.js file

--- js/modalContainerDirective.js
+++ js/modalContainerDirective.js
@@ -2,21 +2,25 @@
     'use strict';
 
     function asModalContainer() {
+        function asModalContainerController($scope) {
+            // If there is a trigger, the modal is initially hidden
+            // If not, the modal is initially visible
+            $scope.modalVisible = !$scope.trigger;
+            // Simple function to show/hide the modal
+            $scope.showModal = function () {
+                $scope.modalVisible = !$scope.modalVisible;
+            };
+        }
+
+        asModalContainerController.$inject = ['$scope'];
+
         return {
             restrict: 'E',
             transclude: true,
             scope: {
                 trigger: '='
             },
-            controller: function ($scope) {
-                // If there is a trigger, the modal is initially hidden
-                // If not, the modal is initially visible
-                $scope.modalVisible = !$scope.trigger;
-                // Simple function to show/hide the modal
-                $scope.showModal = function () {
-                    $scope.modalVisible = !$scope.modalVisible;
-                };
-            },
+            controller: asModalContainerController,
             // Set the directive as a inline tag to prevent design problems
             template: '<span ng-transclude></span>'
         };
